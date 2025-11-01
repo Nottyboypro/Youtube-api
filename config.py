99d37cc1865695f903a4041e5d9c6a35f430ec85
Nottyboypro <nottyboy.vip.@gmail.com>
2025-11-01 16:16:37 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,16 @@
+# No need to import dotenv now
+import os
+
+# 🔥 Direct values set kar diye (yahan apni actual values daal)
+MONGO_DB_URI = "mongodb+srv://jaydipmore74:xCpTm5OPAfRKYnif@cluster0.5jo18.mongodb.net/?retryWrites=true&w=majority"
+API_ID = 24168862  # Telegram API ID (int me likhna)
+API_HASH = "916a9424dd1e58ab7955001ccc0172b3"
+BOT_TOKEN = "8239718956:AAF9cYssUnTbHcPhcmLO8VAaw8xgGR6_7EU"
+CHANNEL_ID = -1002999363928  # Channel ka numeric ID
+CHANNEL_USERNAME = "@NOTTYAPI"
+
+SECRET_KEY_JWT = "change_this_secret"  # JWT secret key
+CONCURRENCY_LIMIT = 10
+PORT = 8000
+ADMIN_USERNAME = "admin"
+ADMIN_PASSWORD = "admin"
